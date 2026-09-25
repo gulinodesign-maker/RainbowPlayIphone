0e9f861cce657527300aa613c2e435aab9428488
gulinodesign-maker <gulinodesign@gmail.com>
2026-09-25 23:54:40 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-/* Music Rainbow Service Worker - Build 2.126 */
-const BUILD = "2.126";
+/* Music Rainbow Service Worker - Build 2.127 */
+const BUILD = "2.127";
 const PRECACHE = `music-rainbow-precache-${BUILD}`;
 const RUNTIME = `music-rainbow-runtime-${BUILD}`;
 
